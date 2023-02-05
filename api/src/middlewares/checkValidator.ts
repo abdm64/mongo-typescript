74206f8ed181b437586b7b7a8651562cbf5ab449
root <root@abdm64-pc>
2023-02-05 23:12:55 +0000

    add new job

--- api/src/middlewares/checkValidator.ts
+++ api/src/middlewares/checkValidator.ts
@@ -8,9 +8,6 @@ export const checkUserInputValidator = (): ValidationChain[] => [
     check('hobby.year', ' year is required').not().isEmpty(),
     check('hobby.name', ' name is required').not().isEmpty(),
     check('hobby.passion', ' passion must be Low Medium High or Very-High').custom((val) => (val in Passion))
-
-    
- 
 ]
 
 export const checkHobbyInputValidator = (): ValidationChain[] => [
